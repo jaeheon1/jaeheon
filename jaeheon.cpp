aeaf5aa1974c62a2ff02df545ea9ca41ad83b2b6
Eripron <87156859+Eripron@users.noreply.github.com>
2022-05-11 19:34:59 +0900

    초기 커밋.

---
+++ jaeheon.cpp
@@ -0,0 +1,56 @@
+﻿
+
+
+
+// i =in put o= out put 
+#include <iostream>
+
+
+
+//객체 지향의 장점 
+// 1. 유지 보수가 용이합니다. 
+// 객체들끼리 상호작용 
+//   머리 
+//팔      팔
+//   몸통
+// 다리    다리
+
+int main()
+{
+	char character = 'A';
+	int value = 100;
+	//float 변수는 소수점 6자릿까지 정확도를 표시합니다. 
+
+	float variable = 6.13213;
+	//double 변수는 소수점 15 자릿까지 정확도를 표시합니다. 
+	double x = 1.111111111;
+
+	// cptr 은 배열의 시작주소
+	char cptr[10];
+	gets_s(cptr, sizeof(cptr));
+	//scanf_s("%s", sizeof(cptr),cptr);
+	//2^2 2에 2승
+	
+	// 소수 
+	//0.375
+	//x   2 
+	//0.750
+	//x   2
+	//1.500
+	//x    2
+	//1.000
+
+	//[d] [a] [r] [e] [r] [a] [d] -프로그램
+	//컴퓨터 내부에서 데미터 입력 ->[     ]   -> 화면 
+	//  cpu                        외부장치  
+
+	std::string G = "count";
+
+	std::cin >> value;
+	std::cout << value << std::endl;
+	std::cout << character<<std::endl;
+	std::cout << variable <<std::endl;
+	std::cout << x << std::endl;
+	std::cout << G << std::endl;
+}
+
