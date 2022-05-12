fa0b747d62466e9dcce02a93e6e365b14bba52eb
Eripron <87156859+Eripron@users.noreply.github.com>
2022-05-12 19:46:32 +0900

    Class

--- jaeheon.cpp
+++ jaeheon.cpp
@@ -1,56 +1,122 @@
 ﻿
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+
+//상위 클래스 =부모 클래스 
+//하위 클래스 =자식클래스
 
 
+class Monster
+{
+	//공개
+public:
 
-// i =in put o= out put 
-#include <iostream>
+	int x;
+	int y;
+	int z;
+	void Death()
+	{
+		HP -= 10;
+		std::cout << HP << std::endl;
+    }
+
+	//보호
+protected:
+
+	int money = 100;
+private:
+	int HP = 100;
 
 
+	void Attack()
+	{
+		std::cout << "Attack" << std::endl;
+	}
+};
 
-//객체 지향의 장점 
-// 1. 유지 보수가 용이합니다. 
-// 객체들끼리 상호작용 
-//   머리 
-//팔      팔
-//   몸통
-// 다리    다리
+class Child_Monster:public Monster
+{
+public:
+	void Speaking()
+	{
+		std::cout << "Hello" << std::endl;
+		std::cout <<money<< std::endl;
+	}
+	
+};
+
+//[]
+//위치 (x,y,z)
+//공격 
+//HP 를 확인할 수 있는 함수 
+struct Object
+{
+
+
+	int x = 0; // 맴버 변수 
+	int y = 0;
+	int z = 0;
+
+	int HP = 100;
+	void Attack() //맴버 함수 
+	{
+		std::cout << "공격" << std::endl;
+	}
+	void Health()
+	{
+		std::cout << "현재 체력은 HP 입니다. " << HP << std::endl;
+	}
+	
+};
+
+//public (건들수있음)
+
+//private (건들수없음)
+
+//struct 는 접근 지정자를 따로 설정 하지 않으면 접근지정자가 public으로 설정되어 있습니다. 
+//class 같은 경우는 모든게 private으로 되어 있어서 접근을 할수없다. 따라서 접근 지정자를 따로 설정 해줘야한다. 
+//protect 는 상속된 객체가 아니면 쓸수가없다. 
+//private 안에 있는건 건들수없지만 함수로 정의를 해서  깍아내리는건 가능하다. 예를 들어서 체력을 깎기.
 
 int main()
 {
-	char character = 'A';
-	int value = 100;
-	//float 변수는 소수점 6자릿까지 정확도를 표시합니다. 
+	Object cube;
 
-	float variable = 6.13213;
-	//double 변수는 소수점 15 자릿까지 정확도를 표시합니다. 
-	double x = 1.111111111;
+	Monster orc;
 
-	// cptr 은 배열의 시작주소
-	char cptr[10];
-	gets_s(cptr, sizeof(cptr));
-	//scanf_s("%s", sizeof(cptr),cptr);
-	//2^2 2에 2승
-	
-	// 소수 
-	//0.375
-	//x   2 
-	//0.750
-	//x   2
-	//1.500
-	//x    2
-	//1.000
 
-	//[d] [a] [r] [e] [r] [a] [d] -프로그램
-	//컴퓨터 내부에서 데미터 입력 ->[     ]   -> 화면 
-	//  cpu                        외부장치  
 
-	std::string G = "count";
+	//std::cout << cube.x << std::endl;
+	//std::cout << cube.y << std::endl;
+	//std::cout << cube.z << std::endl;
 
-	std::cin >> value;
-	std::cout << value << std::endl;.
-	std::cout << character<<std::endl;
-	std::cout << variable <<std::endl;
-	std::cout << x << std::endl;
-	std::cout << G << std::endl;
+
+	// cube의 체력을 0 이하로 만들면 while 문이 종료 되도록 설정합니다. 
+	// 스페이스를 데이미가 10씩 들어가도록 설정합니다.
+	while (1)
+	{
+		/*if (GetAsyncKeyState(VK_SPACE))
+		{
+			Sleep(50);
+			cube.HP -= 10;
+			cube.Health();
+		}
+		if (cube.HP <= 0)
+		{
+			break;
+		}*/
+
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			orc.Death();
+			//orc.Breaking();
+			Sleep(100);
+
+
+
+		}
+
+
+	}
 }
-
