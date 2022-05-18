bc3b176fe8000d1e6fc2e395172e6b3e5fed0cb1
Eripron <87156859+Eripron@users.noreply.github.com>
2022-05-18 19:46:52 +0900

    동적할당

---
+++ Model.h
@@ -0,0 +1,50 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+
+
+class Model
+{
+public:
+
+	Model(int size,int mesh,const char *name) 
+	{
+		m_size = new int(size);// 10 , 100, wizard 
+		                       //m_size ------->[0] [1] [2] [3]4 byte (10)
+		m_mesh = mesh;
+		m_name = name;// 1 byte 메모리 100개 생성
+		
+		std::cout << m_size << std::endl;
+		std::cout << m_mesh << std::endl;
+		std::cout << m_name << std::endl;
+	}
+	~Model()
+	{
+		//동적 할당 메모리를 해제 해야되는데 
+		
+		delete m_size;
+
+		std::cout << "객체가 소멸되었습니다." <<m_name<< std::endl;
+	}
+
+	Model(const Model &value)
+	{
+		m_size=new int();
+		m_mesh=value.m_mesh;
+		m_name=value.m_name;
+
+		std::cout << "복사 생성자 호출" << std::endl;
+	}
+
+
+
+private:
+	int *m_size;
+
+	int m_mesh;
+
+	const char * m_name;
+	
+};
+
--- jaeheon.cpp
+++ jaeheon.cpp
@@ -1,120 +1,32 @@
 ﻿#include<iostream>
-#include"Book.h"
-
-
-
-
-
-class Person
-{
-public:
-	//생성자 
-	Person()
-	{
-		std::cout << "Create Person" << std::endl;
-	}
-
-	~Person()
-	{
-		std::cout << "Delete Person" << std::endl;
-	}
-
-	void Info()
-	{
-		std::cout << age << std::endl;
-		std::cout << weight << std::endl;
-		std::cout << name << std::endl;
-	}
-
-
-
-
-
-	int age;
-	float weight;
-	std::string name;
-
-
-};
-
-
+#include"Model.h"  //Model Class 가 들어옵니다. 
 int main()
 {
+	
+	/*int* ptr = new int(10);
 
-	Book naver;
+	std::cout << *ptr << std::endl;
+	std::cout << ptr << std::endl;
 
-	naver.Read();
+	delete ptr;
+	// stack
+	//ptr ------->[][][][]
+	
+	std::cout << ptr << std::endl;*/
 
-	naver.Write();
+	Model Character(10, 30, "wizard");
 
+	Model Monster = Character;
 
+	
 
-
-
-	//객체 할당
-	/*Person* man = new Person();
-
-	std::cout << sizeof(Person) << std::endl;
-
-	Person girl;
-	Person 이라는 객체는 생성이 되었다? ㅇ  
-
-	man->age = 20;
-	man->weight = 70.5;
-	man->name = "kimGeumsoo";
-	man->Info();
-
-	delete man;
-	*/
+	//std::cout << "Character의 주소:" << &Character << std::endl;
+	//std::cout <<"Monster의 주소: "<< &Monster << std::endl;
 
 
 
-	// 동적 할당 배열
-	    /*  //4byte *5 =20 byte
-	          //[] [] [] []
-	          //[] [] [] []
-	          //[] [] [] []
-	int* ptr = new int[3];
-
-	ptr[0] = 1000;
-	ptr[1] = 2000;
-	ptr[2] = 3000;
-
-	std::cout << sizeof(ptr) << std::endl;
-
-	for (int i = 0; i < 3;i++)
-	{
-		std::cout << ptr[i] << std::endl;
-		std::cout << "ptr변수가 가리키는 주소" << &ptr[i] << std::endl;
-	}
-
-	//동적 할당을 했을 때 배열 형태로 메로리를 할당하게 되면 메모리를 헤제할 때 배열 형태로 해제해주어야합니다. 
-
-
-	delete []ptr;
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << ptr[i] << std::endl;
-		std::cout << "ptr변수가 가리키는 주소" << &ptr[i] << std::endl;
-	}
-
-	// 스택       힙
-	//ptr-------> 
-	//이미 해제된 메모리를 가리키는 포인터 입니다.*/
-
-
-
-	//참조 연산자 
-	/*int variable = 300;
-	int value = 10;
-	int& ref = value;
-	ref = variable;
-	ref = 1000;
-
-	std::cout << "value의 값은 : "<<value << std::endl;
-	std::cout << variable << std::endl;
-	std::cout << "ref의 값은 :"<<ref << std::endl;*/
 
 
 	return 0;
+
 }
